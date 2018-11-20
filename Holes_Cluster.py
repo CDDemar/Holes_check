dd5c7da63c0b1bc837ca55ec74eabd7846a869ac
Cristhyan De Marchena <demarchenac@uninorte.edu>
2018-11-20 00:00:46 -0500

    Update Holes_Cluster.py

--- Holes_Cluster.py
+++ Holes_Cluster.py
@@ -3,12 +3,18 @@ from mpi4py import MPI
 import time
 import math
 
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
 bookmark = 0
 index = 0
 trips = []
 sPoints = []
 neighborhood = []
-neighborhoods = []
+target = 0
+procesing = 1
+start = False
 prom_Lat = 0
 prom_Lng = 0
 prom_Alt = 0
@@ -70,89 +76,116 @@ try:
 except SyntaxError:
 	S = 0.8
 if(go):
-	start = time.time()
-	print('Loading data!')
+    print('Loading data!')
 	tree = ET.parse('raw.xml')
 	root = tree.getroot()
 	print('Data loaded, processing begins!')
 	raw_input('Hit ENTER to continue')
-	# Creando los viajes y asignandoles sus respectivos puntos:
-	for database in root.findall('database'):
-		for child in database:
-			if(bookmark != int(child[1].text)):
-				bookmark = int(child[1].text)
-				if(len(trips) <= 1):
-					trip = Trip(bookmark)
-					if(len(trips) == 0):
-						index = 0
-					else: 
-						index = 1
-					trips.append(trip)
-				else:
-					exist = False
-					for i, trip in enumerate(trips):
-						if(trip.ID == bookmark):
-							exist = True
-							index = i
-							break
-					if(not exist):
-						trip = Trip(bookmark)
-						trips.append(trip)
-						index = len(trips) -1
-			ID = int(child[0].text)
-			Trip_ID = int(child[1].text)
-			TimeStamp = int(child[2].text)
-			Lat = float(child[3].text)
-			Lng = float(child[4].text)
-			Alt = float(child[5].text)
-			Speed = float(child[6].text)
-			Ace = float(child[7].text)
-			x = Point(Lat, Lng, Alt, Ace, ID, Trip_ID, TimeStamp, Speed)
-			trips[index].points.append(x)
-	# Normalizando los puntos por viaje:
-	print('Normalizing all the points per trips!')
-	for trip in trips:
-		trip.normalize()
-	# Creando un vector de puntos y ordenandolo:
-	print('Sorting all the points!')
-	for trip in trips:
-		for point in trip.points:
-			sPoints.append(point)
-	sPoints.sort(key = lambda x: x.Ace, reverse=True)
-	print('Finding neighborhoods!')
-	# P >= S y dist(cand, P) <= R!
-	for point in sPoints:
-		if(point.Ace >= S and not point.processed):
-			neighborhood = []
-			neighborhood.append(point)
-			point.processed = True
-			for candidate in sPoints:
-				d = point.haversine(candidate)
-				if(d <= R and not candidate.processed):
-					neighborhood.append(candidate)
-					candidate.processed = True
-			# Marcar puntos de la misma ruta:
-			for candidate in sPoints:
-				if(point.Trip_ID == candidate.Trip_ID):
-					candidate.processed = True
-			neighborhoods.append(neighborhood)
-	print('Finding holes!')
-	for neighborhood in neighborhoods:
-		for point in neighborhood:
-			prom_Lat = prom_Lat +point.Lat
-			prom_Lng = prom_Lng +point.Lng
-			prom_Alt = prom_Alt +point.Alt
-			prom_Ace = prom_Ace +point.Ace
-		prom_Lat = prom_Lat/len(neighborhood)
-		prom_Lng = prom_Lng/len(neighborhood)
-		prom_Alt = prom_Alt/len(neighborhood)
-		prom_Ace = prom_Ace/len(neighborhood)
-		if(prom_Ace >= S):
-			h = Point(prom_Lat, prom_Lng, prom_Alt, prom_Ace)
-			print(h.Lat, h.Lng, h.Alt, h.Ace)
-			holes.append(h)
-	print('There are', len(holes), 'holes')
-	print('Time:', (time.time() -start))
+	
+    if(rank == 0):
+        initial = time.time()
+        # Creando los viajes y asignandoles sus respectivos puntos:
+        for database in root.findall('database'):
+            for child in database:
+                if(bookmark != int(child[1].text)):
+                    bookmark = int(child[1].text)
+                    if(len(trips) <= 1):
+                        trip = Trip(bookmark)
+                        if(len(trips) == 0):
+                            index = 0
+                        else: 
+                            index = 1
+                        trips.append(trip)
+                    else:
+                        exist = False
+                        for i, trip in enumerate(trips):
+                            if(trip.ID == bookmark):
+                                exist = True
+                                index = i
+                                break
+                        if(not exist):
+                            trip = Trip(bookmark)
+                            trips.append(trip)
+                            index = len(trips) -1
+                ID = int(child[0].text)
+                Trip_ID = int(child[1].text)
+                TimeStamp = int(child[2].text)
+                Lat = float(child[3].text)
+                Lng = float(child[4].text)
+                Alt = float(child[5].text)
+                Speed = float(child[6].text)
+                Ace = float(child[7].text)
+                x = Point(Lat, Lng, Alt, Ace, ID, Trip_ID, TimeStamp, Speed)
+                trips[index].points.append(x)
+	    # Normalizando los puntos por viaje:
+        print('Normalizing all the points per trips!')
+        for trip in trips:
+            trip.normalize()
+        # Creando un vector de puntos y ordenandolo:
+        print('Sorting all the points!')
+        for trip in trips:
+            for point in trip.points:
+                sPoints.append(point)
+        sPoints.sort(key = lambda x: x.Ace, reverse=True)
+	    print('Finding neighborhoods!')
+        # P >= S y dist(cand, P) <= R!
+        start = True
+	    comm.bcast(start, root=0)
+        while (sPoints[target].Ace > S):
+            point = sPoints[target]
+            if(not point.processed):
+                neighborhood = []
+                neighborhood.append(point)
+                point.processed = True
+                for candidate in sPoints:
+                    d = point.haversine(candidate)
+                    if(d <= R and not candidate.processed):
+                        neighborhood.append(candidate)
+                        candidate.processed = True
+                # Marcar puntos de la misma ruta:
+                for candidate in sPoints:
+                    if(point.Trip_ID == candidate.Trip_ID):
+                        candidate.processed = True
+                comm.send(neighborhood, dest=procesing)
+                procesing = (procesing + 1) % size
+                if (procesing == 0): 
+                    procesing = 1
+                target = target + 1
+        for i in range(1,size): 
+            comm.send(False, dest=i)
+        for i in range(1,size):
+            results = comm.recv(source=i)
+		    for x in range(0,len(results)): 
+                holes.append(results[x])
+        print('Huecos:')
+        for h in holes:
+			print(h.Lat, h.Lng, h.Alt, h.Ace)     
+        print('Time:', (time.time() -initial))
+    else:
+        start = comm.bcast(start, root=0)
+        while start:
+            neighborhood = comm.recv(source=0)
+            thread_time = time.time()
+            if isinstance(job, bool):
+                start = False
+            else:
+                prom_Lat = 0
+                prom_Lng = 0
+                prom_Alt = 0
+                prom_Ace = 0
+                for point in neighborhood:
+                    prom_Lat = prom_Lat +point.Lat
+                    prom_Lng = prom_Lng +point.Lng
+                    prom_Alt = prom_Alt +point.Alt
+                    prom_Ace = prom_Ace +point.Ace
+                prom_Lat = prom_Lat/len(neighborhood)
+                prom_Lng = prom_Lng/len(neighborhood)
+                prom_Alt = prom_Alt/len(neighborhood)
+                prom_Ace = prom_Ace/len(neighborhood)
+                if(prom_Ace >= S):
+                    h = Point(prom_Lat, prom_Lng, prom_Alt, prom_Ace)
+                    results.append(h)
+        comm.send(results, dest=0)
 else:
 	print('Syntax error, R must be an int and S must be a float!')
 
